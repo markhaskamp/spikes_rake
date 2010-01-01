d06cc93866e5a777992835a66cb5f51add46ca6b
mrh <mark.haskamp@pinnsg.com>
2010-01-01 11:36:15 -0500

    namespaces

--- railsEnvy/rakeFile.rb
+++ railsEnvy/rakeFile.rb
@@ -1,17 +1,21 @@
 
-desc "This task will purchase your Vodka"
-task :purchaseAlcohol do
-  puts "Purchased Vodka"
-end
+namespace :alcoholic do
 
-desc "This task will drink one too many"
-task :mixDrink => :purchaseAlcohol do
-  puts "Mixed Fuzzy Navel"
-end
+  desc "This task will purchase your Vodka"
+  task :purchaseAlcohol do
+    puts "Purchased Vodka"
+  end
 
-desc "This task will drink one too many"
-task :getSmashed => :mixDrink do
-  puts "Dood, everthing's blurry, can I halff noth'r drinnnk?"
+  desc "This task will drink one too many"
+  task :mixDrink => :purchaseAlcohol do
+    puts "Mixed Fuzzy Navel"
+  end
+
+  desc "This task will drink one too many"
+  task :getSmashed => :mixDrink do
+    puts "Dood, everthing's blurry, can I halff noth'r drinnnk?"
+  end
+
 end
 
 
