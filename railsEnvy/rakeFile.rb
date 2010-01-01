7fe6466eb095de1b573da82244257e480a0a534e
mrh <mark.haskamp@pinnsg.com>
2010-01-01 11:49:39 -0500

    ruby tasks

--- railsEnvy/rakeFile.rb
+++ railsEnvy/rakeFile.rb
@@ -6,7 +6,7 @@ namespace :alcoholic do
     puts "Purchased Vodka"
   end
 
-  desc "This task will drink one too many"
+  desc "This task will mix the drinks"
   task :mixDrink => :purchaseAlcohol do
     puts "Mixed Fuzzy Navel"
   end
@@ -17,5 +17,30 @@ namespace :alcoholic do
   end
 
 end
+
+
+namespace :dirs do
+
+  desc "Create blank directories if they don't already exist"
+  task(:create_directories) do
+
+    # the folders I need to create
+    shared_folders = ["icons", "images", "groups"]
+
+    for folder in shared_folders
+
+      # check to see if it exists
+      if File.exists?(folder)
+        puts "#{folder} exists"
+      else
+        puts "#{folder} doesn't exist so we're creating"
+        Dir.mkdir "#{folder}"
+      end
+    end
+  end
+
+end
+
+
 
 
