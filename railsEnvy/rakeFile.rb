a38861ec30a57782f9f392648b224a61472958f1
mrh <mark.haskamp@pinnsg.com>
2010-01-01 11:30:28 -0500

    document

--- railsEnvy/rakeFile.rb
+++ railsEnvy/rakeFile.rb
@@ -1,12 +1,15 @@
 
+desc "This task will purchase your Vodka"
 task :purchaseAlcohol do
   puts "Purchased Vodka"
 end
 
+desc "This task will drink one too many"
 task :mixDrink => :purchaseAlcohol do
   puts "Mixed Fuzzy Navel"
 end
 
+desc "This task will drink one too many"
 task :getSmashed => :mixDrink do
   puts "Dood, everthing's blurry, can I halff noth'r drinnnk?"
 end
