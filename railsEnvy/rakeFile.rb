684ded2d21be5cbd71ebc6506d085706567b33a2
mrh <mark.haskamp@pinnsg.com>
2010-01-01 11:22:37 -0500

    railsenvy tutorial. initial load.

---
+++ railsEnvy/rakeFile.rb
@@ -0,0 +1,14 @@
+
+task :purchaseAlcohol do
+  puts "Purchased Vodka"
+end
+
+task :mixDrink do
+  puts "Mixed Fuzzy Navel"
+end
+
+task :getSmashed do
+  puts "Dood, everthing's blurry, can I halff noth'r drinnnk?"
+end
+
+
