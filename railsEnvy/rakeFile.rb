4934ca606c6a4148913f42503e2af6a5833024e0
mrh <mark.haskamp@pinnsg.com>
2010-01-01 11:25:59 -0500

    dependencies

--- railsEnvy/rakeFile.rb
+++ railsEnvy/rakeFile.rb
@@ -3,11 +3,11 @@ task :purchaseAlcohol do
   puts "Purchased Vodka"
 end
 
-task :mixDrink do
+task :mixDrink => :purchaseAlcohol do
   puts "Mixed Fuzzy Navel"
 end
 
-task :getSmashed do
+task :getSmashed => :mixDrink do
   puts "Dood, everthing's blurry, can I halff noth'r drinnnk?"
 end
 
