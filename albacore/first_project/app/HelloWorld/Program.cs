92ba35548d88f1a8ad1be2eecbb6353934113d11
Mark Haskamp <mrhask@gmail.com>
2010-05-03 19:55:29 -0400

    added simple albacore spike

---
+++ albacore/first_project/app/HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("hello world!");
+        }
+    }
+}
---
+++ albacore/first_project/test/HelloWorld_Test/Class1.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace HelloWorld_Test
+{
+    [TestFixture]
+    public class Class1
+    {
+
+        [Test]
+        public void The_First_Test() {
+            Assert.AreEqual(42, (6*7));
+        }
+
+        [Test]
+        public void The_Second_Test() {
+            Assert.IsTrue(true);
+        }
+    }
+}
